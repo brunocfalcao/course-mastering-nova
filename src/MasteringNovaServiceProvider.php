87ed9f559dd0261eabb69b4052b13819393e8122
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2022-11-06 15:58:25 +0100

    wip

--- src/MasteringNovaServiceProvider.php
+++ src/MasteringNovaServiceProvider.php
@@ -1,51 +1,18 @@
 <?php
 
-namespace BinanceApi;
+namespace MasteringNova;
 
-use BinanceApi\Commands\GetTickers;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class MasteringNovaServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->importMigrations();
-        $this->registerCommands();
-        $this->loadRoutes();
-        $this->loadViews();
-    }
-
-    protected function loadViews()
-    {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'course');
-    }
-
-    protected function loadRoutes()
-    {
-        /*
-        Route::middleware(['web'])
-             ->group(function () {
-                 include __DIR__.'/../routes/web.php';
-             });
-        */
+        //
     }
 
     public function register()
     {
-        Schema::defaultStringLength(191);
-    }
-
-    protected function importMigrations(): void
-    {
-        //$this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    }
-
-    protected function registerCommands(): void
-    {
-        $this->commands([
-            //GetTickers::class,
-        ]);
+        //
     }
 }
